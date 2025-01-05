a441f3847c5663e541324031aa3f3d151415138d
Hash-Cracker <tspamiitesh@gmail.com>
2025-01-05 11:27:35 +0530

    Adding options for the login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "" // Add the source file for the LoginPage
   ],
   theme: {
     extend: {
@@ -10,6 +11,7 @@ export default {
         'enigma-green': '#22EC08',
         'bg-black': '#111111',
         'bg-enigma-black': '#111111',
+        'login-line': '#1cd41c', // Added for login component by Amiitesh
       },
 
       fontSize: {
@@ -20,7 +22,13 @@ export default {
         glow: [
           "0 0px 20px rgba(255,255, 255, 0.35)",
           "0 0px 65px rgba(255, 255,255, 0.2)"
-        ]
+        ],
+        'enigma-glow': [ // Glow for Enigma25 text in Login Page by Amiitesh
+          '0 0 10px #1cd41c',
+          '0 0 20px #1cd41c',
+          '0 0 30px #1cd41c',
+          '0 0 40px #1cd41c',
+        ],
       },
 
       fontFamily: {
@@ -29,9 +37,25 @@ export default {
         'neuebit': ['NeueBit', 'sans-serif'],
         'montreal-medium': ['Montreal-Medium', 'sans-serif'],
         'montreal-book': ['Montreal-Book', 'sans-serif'],
+        'orbitron': ['Orbitron', 'sans-serif'] // Adding Orbitron Font by Amiitesh
+      },
+      keyframes: { // Keyframes for the login page by Amiitesh
+        'roaming-line': {
+          '0%': { borderColor: 'transparent #1cd41c transparent transparent' },
+          '25%': { borderColor: 'transparent transparent #1cd41c transparent' },
+          '50%': { borderColor: 'transparent transparent transparent #1cd41c' },
+          '75%': { borderColor: '#1cd41c transparent transparent transparent' },
+        },
+        animation: {
+        'roaming-line': 'roaming-line 1s linear infinite', // By Amiitesh for the animation 
       }
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
 
