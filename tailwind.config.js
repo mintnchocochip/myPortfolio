3e698cfd045aa6d386fcc3d0a44671724dca3957
Hash-Cracker <tspamiitesh@gmail.com>
2025-01-05 12:30:16 +0530

    Updating tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/login/*.{jsx}",
-    "./src/signup/*.{jsx}"
+    "./src/login/*.jsx",
+    "./src/signup/*.jsx"
 
   ],
   theme: {
