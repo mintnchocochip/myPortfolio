8db16fd5e29c6095a1638dc8822a486cc6c2bb89
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-01-14 16:00:44 +0530

    update: text size, fonts and responsitivity

--- src/components/hamburger.jsx
+++ src/components/hamburger.jsx
@@ -34,15 +34,15 @@ export default function Hamburger() {
 
     return (
         <div className="flex justify-end">
-            <div className='hidden lg:flex'>
-                        <ul className='flex flex-1 justify-between gap-11 p-4'>
-                            <li><Link to="/prizes" onClick={handleLinkClick}><AnimatedText className="hover:p-2 text-4xl font-light text-white  hover:bg-enigma-green hover:text-black" text="Prizes"/></Link></li>
-                            <li><Link to="/timeline" onClick={handleLinkClick}><AnimatedText className="hover:p-2 text-4xl font-light text-white hover:bg-enigma-green hover:text-black" text="Timeline"/></Link></li>
-                            <li><Link to="/rules" onClick={handleLinkClick}><AnimatedText className="hover:p-2 text-4xl font-light text-white hover:bg-enigma-green hover:text-black" text="Rules"/></Link></li>
-                            <li><Link to="/about" onClick={handleLinkClick}><AnimatedText text="About" className="hover:p-2 text-4xl font-light text-white hover:bg-enigma-green hover:text-black"/></Link></li>
+            <div className='hidden sm:flex'>
+                        <ul className='flex flex-1 justify-between gap-11 p-4 font-neuebit uppercase'>
+                            <li><Link to="/prizes" onClick={handleLinkClick}><AnimatedText className="hover:p-2 text-2xl text-white  hover:bg-enigma-green hover:text-black" text="Prizes"/></Link></li>
+                            <li><Link to="/timeline" onClick={handleLinkClick}><AnimatedText className="hover:p-2 text-2xl text-white hover:bg-enigma-green hover:text-black" text="Timeline"/></Link></li>
+                            <li><Link to="/rules" onClick={handleLinkClick}><AnimatedText className="hover:p-2 text-2xl text-white hover:bg-enigma-green hover:text-black" text="Rules"/></Link></li>
+                            <li><Link to="/about" onClick={handleLinkClick}><AnimatedText text="About" className="hover:p-2 text-2xl text-white hover:bg-enigma-green hover:text-black"/></Link></li>
                         </ul>
         </div>
-            <div ref={menuRef} className={` lg:hidden flex flex-col justify-end text-black bg-enigma-green font-neuebit uppercase ${isOpen ? 'w-full' : 'w-auto'}`}>
+            <div ref={menuRef} className={` sm:hidden flex flex-col justify-end text-black bg-enigma-green font-neuebit uppercase ${isOpen ? 'w-full' : 'w-auto'}`}>
                 <div className="hamburger-icon cursor-pointer text-2xl select-none text-right p-4 flex items-center justify-end" onClick={toggleMenu}>
                     <AnimatedText text="MENU" className="mr-2"/>
                     <span className="text-xl mt-1">&#9776;</span> {/* Hamburger icon */}
