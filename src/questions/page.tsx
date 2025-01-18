f3eb967211dc7493b3b8ff66c96f6fa742aad4a3
Aayush Shukla <oreus.aks@gmail.com>
2025-01-18 23:21:49 +0530

    intialize ctfs page and a route

---
+++ src/questions/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CustomHeader from "../components/CustomHeader";
+
+const CTFs = () => {
+  return (
+    <div className="w-screen h-screen">
+      <CustomHeader />
+      <div className="font-neuebit">CTFs</div>
+    </div>
+  );
+};
+
+export default CTFs;
