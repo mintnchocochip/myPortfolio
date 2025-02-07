a3d565989ffae08f33253b9691a599f5d6489f69
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-07 10:38:34 +0530

    cleanup

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,28 +15,20 @@ import Content from "./components/content.jsx";
 function Home() {
   return (
     <div className='py-2'>
-      {/* <MovableSquare /> */}
       {/* <div className="h-screen"></div> */}
       {/* <ErtdfgcvbBG className='fixed z-0'/> */}
-
-        <Join className='z-20'/>
-        
-        <nav className="sticky top-0 flex justify-between w-full px-10 py-5 z-20">
-          <div className='text-5xl font-neuebit text-left pt-1'>ENIGMA</div>
-          <Hamburger className='fixed right-0'/>
-        </nav>
+      <Join className='z-20'/>
+      
+      <nav className="sticky top-0 flex justify-between w-full px-10 py-5 z-20 mix-blend-difference">
+        <div className='text-5xl font-neuebit text-left pt-1'>ENIGMA</div>
+        <Hamburger className='fixed right-0'/>
+      </nav>
 
       <HeroSection/>
-        <Content />
-        {/*<div className="h-screen"></div>*/}
-        <div className="relative">
-          <AnimatedText text="TIMELINE" className="font-neuebit uppercase text-9xl z-50" customText='グミヸ✨' time={2} preStyle='font-neuebit uppercase text-9xl text-yellow-500 z-50'/>
-        </div>
-      <Scene className="fixed top-0"/>
+      <Content />
 
-        {/* <Timeline className='z-10'/> */}
+      <Scene className="fixed top-0"/>
         <Footer />
-        {/* <BackgroundEffect /> */}
     </div>
   );
 }
