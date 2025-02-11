95f2274a8ba46401ca45a8d596c51e399ed2e8fd
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-12 01:46:30 +0530

    update: hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,37 +1,41 @@
-import React from 'react'
+import React from 'react';
 import AnimatedText from './text';
 import { Element } from "react-scroll";
 
 const HeroSection = () => {
   return (
-      <>
-        <Element name="home">
-        <section className='h-[85vh] '>
-            <div id="container" className='h-[75vh] flex justify-left items-center p-4 z-50'>
-            <div id='hero' className='text-7xl font-neuebit text-center z-50 md:text-8xl'>
-            <div className="flex flex-wrap gap-x-4 p-2 md:p-0 max-w-full justify-center">
-              <AnimatedText text="ENIGMA," preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-              <AnimatedText text="DO" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-              <AnimatedText text="YOU" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-              <AnimatedText text="WANT" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-              <AnimatedText text="TO" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-              <AnimatedText text="FEEL" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-              <AnimatedText text="THE" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-              <AnimatedText text="ULTIMATE" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-              <AnimatedText text="EVENT" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-              <AnimatedText text="EVER" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-              <AnimatedText text="OF" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-              <AnimatedText text="YOUR" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-              <AnimatedText text="LIFE?" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-3 px-3 cursor-cell'/>
-            </div>
-              {/* ENGIMA, DO YOU WANT TO FEEL THE ULTIMATE EVENT EVER OF YOUR LIFE ? */}
+    <>
+      <Element name="home">
+        <section className='h-[85vh]'>
+          <div id="container" className='h-[75vh] flex justify-left items-center p-4 z-50'>
+            <div id='hero' className='text-7xl font-neuebit text-center z-50 md:text-8xl flex justify-between w-full gap-6 md:flex-row flex-col md:items-center'>
+              <AnimatedText text="ENIGMA" className="text-[2em] md:text-[2em]" preStyle='text-black text-[2em] mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell w-full'/>
+              <div className="flex flex-wrap flex-col gap-x-4 p-2 md:p-0 max-w-full md:justify-end align-middle w-full gap-y-0">
+                <div className="flex flex-wrap gap-x-8 w-full md:justify-end">
+                  <AnimatedText text="FIGHT" className="text-[0.6em] md:text-[1em]" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell'/>
+                  <AnimatedText text="TO" className="text-[0.6em] md:text-[1em]" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell'/>
+                  <AnimatedText text="SURVIVE" className="text-[0.6em] md:text-[1em]" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell'/>
+                </div>
+                <div className="flex flex-wrap gap-x-8 w-full md:justify-end">
+                  <AnimatedText text="ALL'S" className="text-[0.6em] md:text-[1em]" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell'/>
+                  <AnimatedText text="FAIR" className="text-[0.6em] md:text-[1em]" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell'/>
+                  <AnimatedText text="IN" className="text-[0.6em] md:text-[1em]" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell'/>
+                  <AnimatedText text="LOVE" className="text-[0.6em] md:text-[1em]" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell'/>
+                  <AnimatedText text="AND" className="text-[0.6em] md:text-[1em]" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell'/>
+                  <AnimatedText text="WAR" className="text-[0.6em] md:text-[1em]" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell'/>
+                </div>
+                <div className="flex flex-wrap gap-x-8 w-full md:justify-end">
+                  <AnimatedText text="THIS" className="text-[0.6em] md:text-[1em]" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell'/>
+                  <AnimatedText text="IS" className="text-[0.6em] md:text-[1em]" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell'/>
+                  <AnimatedText text="WAR" className="text-[0.6em] md:text-[1em]" preStyle='text-black mix mix-blend-difference bg-enigma-green pb-0 md:pb-3 px-3 cursor-cell'/>
+                </div>
+              </div>
             </div>
           </div>
         </section>
-        </Element>
-
-      </>
-  )
+      </Element>
+    </>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
