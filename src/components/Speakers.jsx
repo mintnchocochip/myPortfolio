e22c9b940927aa3026a55ebd90a395cc6ff85d0d
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-12 01:56:52 +0530

    add: ert's donut

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -8,10 +8,10 @@ function Speakers() {
         <div className="text-left px-10 z-20">
         <AnimatedText
             text="SPEAKERS"
-            className="font-neuebit uppercase text-9xl z-50 cursor-pointer w-screen text-left"
+            className="font-neuebit uppercase text-6xl md:text-8xl lg:9xl z-50 cursor-pointer w-screen text-left"
             customText='_-'
             time={2}
-            preStyle='font-neuebit uppercase text-9xl text-yellow-500 z-50 cursor-pointer text-left'
+            preStyle='font-neuebit uppercase text-6xl md:text-8xl lg:9xl text-yellow-500 z-50 cursor-pointer text-left'
             />
         </div>
         <Speaker name="Yeyati Prasher" subname="Linux Basics" values={{topic1: "description1", topic2: "description2", topic3: "description3", topic4: "description4", topic5: "description5", topic6: "description6", topic7: "description7", topic8: "description8",}} ></Speaker>
