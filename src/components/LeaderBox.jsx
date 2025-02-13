d802c0539efc5b27f6a6f1a84a2a90bb3c53ad2b
Abhinav Anil <abhinav.anil.2k5@gmail.com>
2025-02-13 17:51:30 +0530

    fix: add animations to leaderbox text

--- src/components/LeaderBox.jsx
+++ src/components/LeaderBox.jsx
@@ -1,17 +1,29 @@
+import AnimatedText from "./text";
+
 const LeaderBox = ({ teamName, teamPoints, ctfsSolved, number }) => {
   return (
     <div className="flex mt-10">
       <div className="border-2 font-neuebit border-zinc-500 p-5 w-96 flex flex-col justify-between h-96">
-        <h3 className="text-xl mb-auto">NO.{number}</h3>
-        <h4 className="text-enigma-green text-left text-2xl">{teamName}</h4>
+        <h3 className="text-xl mb-auto">
+          <AnimatedText text={`NO.${number}`} className="text-xl mb-auto" />
+        </h3>
+        <h4 className="text-enigma-green text-left text-2xl">
+          <AnimatedText text={teamName} />
+        </h4>
         <hr className="border-none h-0.5 bg-zinc-600 my-0" />
         <div className="flex justify-between">
-            <p>TEAM POINTS</p>
-            <p className="text-enigma-green">{teamPoints}</p>
+          <AnimatedText text={"TEAM POINTS"} />
+          <AnimatedText
+            text={teamPoints.toString()}
+            className="text-enigma-green"
+          />
         </div>
         <div className="flex justify-between">
-            <p>CTFS SOLVED</p>
-            <p className="text-enigma-green">{ctfsSolved}</p>
+          <AnimatedText text={"CTFS SOLVED"} />
+          <AnimatedText
+            text={ctfsSolved.toString()}
+            className="text-enigma-green"
+          />
         </div>
       </div>
     </div>
