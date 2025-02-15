4ad959b11101cce4735405c10fbc3c62351783b2
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-15 16:05:57 +0530

    gave the baby a gun

--- src/assets/functions/CTF_type.tsx
+++ src/assets/functions/CTF_type.tsx
@@ -1,12 +1,21 @@
-export default function CTF_type(num: number) {
-  const types: string[] = ["Web", "OSINT", "Binary", "Reversing", "Forensics", "Cryptography", "Miscellaneous"];
-  const binary: string = num.toString(2).padStart(7, '0');
-  const type: string[] = [];
-  for (let i = 0; i < binary.length; i++) {
-    if (binary[i] === '1') {
-      type.push(types[i]);
+const challengeTypes = [
+  "Miscellaneous",
+  "Forensics",
+  "Web",
+  "Binary",
+  "Reversing",
+  "Crypto",
+  "OSINT",
+];
+
+function getTypesFromMask(mask: number) {
+  const result: string[] = [];
+
+  for (let i = 0; i < challengeTypes.length; i++) {
+    if (mask & (1 << i)) {
+      result.push(challengeTypes[i]);
     }
   }
 
-  return type;
+  return result;
 }
