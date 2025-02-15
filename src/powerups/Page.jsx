41e13fbf62c7845e5316937d31dfca94581f7b20
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-15 19:16:55 +0530

    add: team page

---
+++ src/powerups/Page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/powerups/powerup-panel.tsx';
+
+function PowerPage() {
+  return (
+    <Layout className="h-screen w-screen"/>
+  );
+}
+
+export default PowerPage;
