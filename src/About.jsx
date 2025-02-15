dee5dfde683676b5f6d0a2b019900a43f54e0903
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-15 19:55:12 +0530

    update: speakers section

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import Speakers from './components/Speakers'
+import React, { useEffect } from 'react';
+import Speakers from './components/Speakers';
 
 const About = () => {
+  useEffect(() => {
+    const hasReloaded = localStorage.getItem('hasReloaded');
+    if (!hasReloaded) {
+      localStorage.setItem('hasReloaded', 'true');
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    }
+  }, []);
+
   return (
     <div className="">
       <Speakers />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
